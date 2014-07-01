eb4ccd744a4b10d13cf45e7cc73398699b811cbc
Steve Caldwell <steve@crosstek.net>
2014-07-01 00:11:41 -0400

    Initial Commit

---
+++ StrapMetrics/StrapMetrics.h
@@ -0,0 +1,13 @@
+//
+//  StrapMetrics.h
+//  StrapMetrics
+//
+//  Created by scald on 7/1/14.
+//  Copyright (c) 2014 strap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StrapMetrics : NSObject
+
+@end
---
+++ StrapMetrics/StrapMetrics.m
@@ -0,0 +1,13 @@
+//
+//  StrapMetrics.m
+//  StrapMetrics
+//
+//  Created by scald on 7/1/14.
+//  Copyright (c) 2014 strap. All rights reserved.
+//
+
+#import "StrapMetrics.h"
+
+@implementation StrapMetrics
+
+@end
